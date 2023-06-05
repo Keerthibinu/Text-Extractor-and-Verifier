b754ec3a5ffdd2f755ff37205dc54fbf51a02d68
Keerthi Binu <keerthibinu2507@gmail.com>
2023-06-05 14:40:56 +0000

    ner&fuzzywuzzy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+"""
+Module-level docstring: This module contains functions for image processing.
+"""
+
 import spacy
-import cv2
+from PIL import Image
 from paddleocr import PaddleOCR
 from fuzzywuzzy import fuzz
 
@@ -10,7 +14,7 @@ dictionary = {'Federal Institute Of Science And Technology': '1',
               'Keerthi Binu': '2',
               'FISAT': '3'}
 
-img = cv2.imread('/home/keerthi/img.png')
+img = Image.open('/home/keerthi/img4.png')
 ocr = PaddleOCR()
 result = ocr.ocr(img)
 
@@ -20,20 +24,19 @@ matched_entities = set()
 
 for i in range(len(result[0])):
     s = result[0][i][1][0]
-    l = s.split(" ")
-    for word in l:
-        for key in dictionary.keys():
-            if fuzz.token_set_ratio(word, key) == 100:  # to check similarity ratio
+    k = s.split(" ")
+    for word in k:
+        for key in dictionary:
+            if fuzz.token_set_ratio(word, key) == 100:
                 matched_phrases.add(key)
 
     doc_lg = nlp_lg(s)
     for ent in doc_lg.ents:
         if ent.text in matched_phrases:
-            matched_entities.add((ent.text,ent.label_))
-
+            matched_entities.add((ent.text, ent.label_))
 # Print the matched phrases and entities
 for phrase in matched_phrases:
-    print(f"{phrase} is present")  
+    print(f"{phrase} is present")
 
-for entity,label in matched_entities:
+for entity, label in matched_entities:
     print(f"{entity} is found with label {label}")
